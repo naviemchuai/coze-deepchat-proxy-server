cf53e703410100cc4e389d8fdb1a6e151bc8355e
Ivan <tuanhiepnguyen2002@gmail.com>
2024-05-19 16:58:53 +0700

    removed unnessesary handlings

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ load_dotenv()
 app = Flask(__name__)
 
 # this will need to be reconfigured before taking the app to production
-cors = CORS(app, resources={r"/*": {"origins": ["http://localhost:3000", "http://localhost:3000", "http://coze-deepchat-proxy-server-production.up.railway.app"]}})
+cors = CORS(app, resources={r"/*": {"origins": ["http://localhost", "http://localhost:3000", "http://coze-deepchat-proxy-server-production.up.railway.app"]}})
 
 # ------------------ EXCEPTION HANDLERS ------------------
 
@@ -31,25 +31,18 @@ def handle_exception(e):
 # ------------------ OPENAI API ------------------
 
 open_ai = OpenAI()
-
-@app.route("/1938", methods=["POST", "OPTIONS"])
+@app.route("/1938", methods=["POST"])
 def course_1938():
-    if request.method == 'OPTIONS':
-        return '', 200
     body = request.json
     return open_ai.chat(body, botID="7342365921484292098")
 
-@app.route("/1980", methods=["POST", "OPTIONS"])
+@app.route("/1980", methods=["POST"])
 def course_1980():
-    if request.method == 'OPTIONS':
-        return '', 200
     body = request.json
     return open_ai.chat(body, botID="7342365921484292098")
 
-@app.route("/1977", methods=["POST", "OPTIONS"])
+@app.route("/1977", methods=["POST"])
 def course_1977():
-    if request.method == 'OPTIONS':
-        return '', 200
     body = request.json
     return open_ai.chat(body, botID="7342365921484292098")
 
